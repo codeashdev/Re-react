ea1505290fec2e9482c3417a00af04dc58e4be5f
Ash <lolash420@gmail.com>
2023-03-23 14:13:41 +0300

    Adds function components and setState hook

--- src/index.js
+++ src/index.js
@@ -3,21 +3,46 @@
 import Rereact from "./rereact/Rereact"
 
 /** @jsx Rereact.createElement */
-const container = document.getElementById("root")
 
-const updateValue = e => {
-  rerender(e.target.value)
-}
 
-const rerender = value => {
+// const updateValue = e => {
+//   rerender(e.target.value)
+// }
+
+// const rerender = value => {
  
-  const element = (   
+//   const element = (   
+//     <div>
+//       <h1>Rereact {value}</h1>
+//       <input onInput={updateValue} value={value} />
+//     </div>
+//   )
+//   Rereact.render(element, container)
+// }
+
+// rerender("Works")
+
+
+function App(){
+  const [state] = Rereact.useState("Works");
+  return (
     <div>
-      <h1>Rereact {value}</h1>
-      <input onInput={updateValue} value={value} />
+        <h1>Rereact {state} </h1>
     </div>
-  )
-  Rereact.render(element, container)
+  );
 }
 
-rerender("Works")
+function Counter() {
+  const [state, setState] = Rereact.useState(1);
+  return (
+    <div>
+      <h1> 
+        Count: {state}
+      </h1>
+      <button onClick={() => setState(c => c + 1)}>Count</button>
+    </div>
+  );
+}
+const element =<div> <App /><Counter/></div>;
+const container = document.getElementById("root")
+Rereact.render(element, container);
